6cf901c4ca72acda4ee27786225f07468d562a3f
Ismael <rivas.ivan2018@gmail.com>
2022-06-07 21:42:48 -0600

    comensado las a trabajar

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,11 @@
+const producto = 'Monitor 20 pulgadas';
+
+console.log(producto);
+
+// Conocer la cantidad de letras de la cadena de texto
+
+console.log(producto.length);
+
+console.logo(producto.indexOf('Tablet'));
+console.logo(producto.indexOf('Monitor'));
+console.logo(producto.indexOf('monitor'));
